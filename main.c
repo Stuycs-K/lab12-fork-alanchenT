bbb5ecc338b502aa517c4dad45a41ea6c7448728
Alan Chen <achen50@stuy.edu>
2024-11-25 15:28:26 -0500

    Reading dev random to generate random numbers

--- main.c
+++ main.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
 #include <fcntl.h>
 #include <sys/wait.h>
 
-int main() {
+void throw_error() {
+    printf("[ERROR %d]: %s\n", errno, strerror(errno));
+    exit(errno);
+}
 
+int get_random_int() {
+    int handle = open("/dev/random", O_RDONLY, 0);
+
+    if (handle == -1) {
+        throw_error();
+    }
+
+    int random_value;
+    read(handle, &random_value, sizeof(int));
+
+    close(handle);
+
+    return random_value;
+}
+
+int main() {
+    printf("%d\n", get_random_int());
     return 0;
 }
